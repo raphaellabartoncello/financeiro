b107b528deab13113fabe7777d87671ad0ffd3f7
Raphaella Bartoncello <rbartoncello@ibm.com>
2022-06-08 18:59:30 -0300

    init project

---
+++ src/test/java/br/com/financeiro/despesa/DespesaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financeiro.despesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
